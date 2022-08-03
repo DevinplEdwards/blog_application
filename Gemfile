a4502612dc097839c9f5cbafc5156c2ae88786dc
devinpledwards <devinpledwards@live.com>
2022-08-03 00:59:29 -0400

    correct devise functionality including navbar; added view count

--- Gemfile
+++ Gemfile
@@ -60,3 +60,8 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+#added gems
+gem 'dotenv-rails', groups: [:development, :test]
+
+gem "ruby-lsp", "~> 0.0.4", :group => :development
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_19_195816) do
+ActiveRecord::Schema.define(version: 2022_08_03_034343) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "views", default: 0
   end
 
 end
